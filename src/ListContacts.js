99563206a63033df453333f25c1c9989c31f953e
Abraham-Ragab <Abrahammoustafa@hotmail.co.uk>
2018-08-24 12:37:15 +0200

    update to link

--- src/ListContacts.js
+++ src/ListContacts.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom'
 import PropTypes from 'prop-types'
 import escapeRegExp from 'escape-string-regexp'
 import sortBy from 'sort-by'
@@ -39,11 +40,10 @@ class ListContacts extends Component {
             value={query}
             onChange={(event) => this.updateQuery(event.target.value)}
           />
-          <a
-            href='#create'
-            onClick={this.props.onNavigate}
+          <link
+            to='/create'
             className='add-contact'
-            >Add Contact</a>
+            >Add Contact</link>
         </div>
         {showingContacts.length !== contacts.length && (
           <div className='showing-contacts'>
